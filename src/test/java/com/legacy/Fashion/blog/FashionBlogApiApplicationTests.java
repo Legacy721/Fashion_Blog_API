7eaf5dd06350bb8d8a1b5c6a439c35306d6ef620
Legacy721 <innocentenoch9@gmail.com>
2022-07-26 17:25:56 +0100

    changes

--- src/test/java/com/legacy/Fashion/blog/FashionBlogApiApplicationTests.java
+++ src/test/java/com/legacy/Fashion/blog/FashionBlogApiApplicationTests.java
@@ -1,13 +1,13 @@
-package com.legacy.Fashion.blog;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class FashionBlogApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.legacy.Fashion.blog;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class FashionBlogApiApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
